640637c972133843756f97f057d38c48d179e941
Andy Agus <Redagus@gmail.com>
2020-06-28 15:35:37 -0400

    Done.

--- app/controllers/dogs_controller.rb
+++ app/controllers/dogs_controller.rb
@@ -1,2 +1,18 @@
 class DogsController < ApplicationController
+		
+	before_action :find_dog, only: [:show, :edit, :update, :delete, :destroy]
+
+	def index
+		@dogs = Dog.all
+	end
+
+	def show
+	end
+
+	private 
+
+	def find_dog
+		@dog = Dog.find(params[:id])
+	end
+
 end
