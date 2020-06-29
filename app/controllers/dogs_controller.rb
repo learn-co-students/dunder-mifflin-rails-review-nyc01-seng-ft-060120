dd33df41bff539e0a78983eabfa6b8a86d4ea02b
scubaguba345 <brandon.gubitosa@gmail.com>
2020-06-29 13:45:40 -0400

    almost done

--- app/controllers/dogs_controller.rb
+++ app/controllers/dogs_controller.rb
@@ -1,2 +1,48 @@
 class DogsController < ApplicationController
+    before_action :dogs_method, only: [:show, :edit, :update]
+
+    def index
+        @dogs = Dog.all
+    end
+
+    def show
+
+    end
+
+    def new
+        @dog = Dog.new
+    end
+
+    def create
+        @dog = Dog.create(dog_params)
+        if @dog.valid?
+            redirect_to dog_path(@dog)
+        else      
+            flash[:my_errors] = @dog.errors.full_messages
+            redirect_to new_dog_path
+        end 
+    end
+
+    def edit
+
+    end
+
+    def update
+        if @dog.update(dog_params)
+            redirect_to dog_path(@dog)
+        else     
+            flash[:my_errors] = @dog.errors.full_messages
+            redirect_to new_dog_path 
+        end
+    end
+
+    private
+
+    def dog_params
+        params.require(:dog).permit(:name, :breed, :age, :employee_id)
+    end
+
+    def dogs_method
+        @dog = Dog.find(params[:id])
+    end
 end
--- app/controllers/employees_controller.rb
+++ app/controllers/employees_controller.rb
@@ -1,2 +1,49 @@
 class EmployeesController < ApplicationController
+    before_action :employees_method, only: [:show, :edit, :update]
+
+    def index
+        @employees = Employee.all
+    end
+
+    def show
+
+    end
+
+    def new
+        @employee = Employee.new
+    end
+
+    def create
+        @employee = Employee.create(employee_params)
+        if @employee.valid?
+            redirect_to employee_path(@employee)
+        else      
+            flash[:my_errors] = @employee.errors.full_messages
+            redirect_to new_employee_path
+        end 
+    end
+
+    def edit
+
+    end
+
+    def update
+        if @employee.update(employee_params)
+            redirect_to employee_path(@employee)
+        else     
+            flash[:my_errors] = @employee.errors.full_messages
+            redirect_to edit_employee_path 
+        end
+    end
+
+    private
+
+    def employee_params
+        params.require(:employee).permit(:first_name, :last_name, :alias, :title, :office, :img_url, :dog_id)
+    end
+
+    def employees_method
+        @employee = Employee.find(params[:id])
+    end
 end
+
