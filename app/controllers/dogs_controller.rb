6e3d01d5ff0d433c34367d255aca905d2feca785
mhdhassan99 <mhassan.work99@gmail.com>
2020-06-28 22:48:22 -0400

    Done.

--- app/controllers/dogs_controller.rb
+++ app/controllers/dogs_controller.rb
@@ -1,2 +1,14 @@
 class DogsController < ApplicationController
+
+    def index 
+        @dogs = Dog.all
+    end
+
+    def show 
+        @dog = Dog.find(params[:id])
+    end
+
+    def dog_params(*args)
+        params.require(:dog).permit(*args)
+    end
 end
