fb461e5c114f3edbb42ef74a5d5a0724b40be80b
MaleehaBhuiyan <maleehabhuiyan@baruchmail.cuny.edu>
2020-06-30 18:55:09 -0400

    Done.

--- app/models/dog.rb
+++ app/models/dog.rb
@@ -1,2 +1,8 @@
 class Dog < ApplicationRecord
+   has_many :employees 
+
+   def employee_finder 
+      self.employees.map {|employee| "#{employee.first_name} #{employee.last_name}"}
+   end 
+
 end
